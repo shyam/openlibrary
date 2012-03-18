813ed561a5e6ef7527d5e111212416c8ad54f767
Krishnaswamy Subramanian <jskswamy@gmail.com>
2012-03-18 16:24:00 +0530

    Removing prototype.js and effect.js instead using jquery since its needed by backbone

--- public/js/app.js
+++ public/js/app.js
@@ -4,41 +4,9 @@ var maxWaitTime = 5;
 var waitTime = 0;
 
 var Reservation = Backbone.Model.extend({
-  initialize: function() {
-    this._bookDom = $("book");
-    this._userDom = $("user");
-    this._confirmationDom = $("confirmation");
-    this._waitNotification = $("wait");
-    this.reset();
-  },
   defaults: {
     user: "",
     isbn: ""
-  },
-  _bindUserInputs: function() {
-    $$("input.barcode").each(function(element) {
-      element.purge();
-      element.value = "";
-      element.observe("keypress", function(e){
-        element = e.element();
-        if (e.keyCode == 13) this.set(element.readAttribute("data-input-type"), element.value);
-      }.bind(this));
-    }, this);
-  },
-  _highlight: function(section) {
-    section.appear({afterFinish: function(){
-      this.focus(section);
-    }.bind(this)});
-    section.highlight({startcolor: '#ffff99', endcolor: '#EEEEEE', queue: 'end'});
-  },
-  _updateWaitTime: function(value) {
-    this._waitNotification.update(value);
-  },
-  focus: function(section) {
-    section.select("input.barcode").all(function(element) {
-      element.value = "";
-      element.focus();
-    });
   },
   validate: function(attrs) {
     if (attrs.isbn && !isbnRegex.test(attrs.isbn))
@@ -46,43 +14,90 @@ var Reservation = Backbone.Model.extend({
     else if (attrs.user && !userRegex.test(attrs.user))
       return "Invalid EmployeeID";
   },
-  getBookDetails: function() {
-    this._confirmationDom.fade({afterFinish: function() {
-      this._updateWaitTime(maxWaitTime);
-    }.bind(this)});
-    this._highlight(this._bookDom);
-  },
-  getUserDetails: function() {
-    this._bookDom.fade();
-    this._highlight(this._userDom);
-  },
-  showConfirmation: function() {
-    this._userDom.fade();
-    this._highlight(this._confirmationDom);
-    this.timerId = setInterval(function() {
-      this._updateWaitTime(maxWaitTime - waitTime);
-      waitTime += 1;
-      if (waitTime >= maxWaitTime) {
-        waitTime = 0;
-        clearInterval(this.timerId);
-        this.trigger("confirmed");
-      }
-    }.bind(this), 1000);
-  },
-  reset: function() {
-    this._bindUserInputs();
-    this.getBookDetails();
+  read: function() {
+    this.trigger("read");
   }
 });
 
+var ReservationView = Backbone.View.extend({
+  events: {
+    "keypress input.barcode": "save"
+  },
+  _highlight: function() {
+    //section.highlight({startcolor: '#ffff99', endcolor: '#EEEEEE', queue: 'end'});
+  },
+  focus: function() {
+    barcodeInput = $(this.el).find("input.barcode");
+    barcodeInput.val("");
+    barcodeInput.focus();
+  },
+  save: function(event) {
+    element = event.target;
+    if (event.keyCode == 13) this.model.set($(element).attr("data-input-type"), element.value);
+  },
+  hide: function() {
+    $(this.el).fadeOut();
+  },
+  show: function() {
+    $(this.el).fadeIn();
+    this.focus();
+  }
+})
+
+var BookView = ReservationView.extend({
+  initialize: function(options) {
+    this.setElement($("#book"));
+    this.model.on("read", $.proxy(this.show, this));
+    this.model.on("change:isbn", $.proxy(this.hide, this));
+  }
+});
+
+var UserView = ReservationView.extend({
+  initialize: function(options) {
+    this.setElement($("#user"));
+    this.model.on("change:user", $.proxy(this.hide, this));
+    this.model.on("change:isbn", $.proxy(this.show, this));
+  }
+})
+
+var ConfirmationView = ReservationView.extend({
+  initialize: function(options) {
+    this.setElement($("#confirmation"));
+    this.model.on("change:user", $.proxy(this.startTimer, this));
+    this.model.on("read", $.proxy(this.hide, this));
+    this._updateWaitTime(maxWaitTime);
+  },
+  _updateWaitTime: function(value) {
+    $(this.el).find("#wait").html(value)
+  },
+  _updateTime: function() {
+    waitTime += 1;
+    this._updateWaitTime(maxWaitTime - waitTime);
+    if (waitTime >= maxWaitTime) {
+      waitTime = 0;
+      clearInterval(this.timerId);
+      this.model.read();
+      this._updateWaitTime(maxWaitTime);
+    }
+  },
+  startTimer: function() {
+    this.show();
+    this.timerId = setInterval($.proxy(this._updateTime, this), 1000);
+  }
+})
+
 var reservation;
+var bookView;
+var userView;
+var confirmationView;
 
 function newReservation() {
   reservation = new Reservation();
+  bookView = new BookView({model: reservation});
+  userView = new UserView({model: reservation});
+  confirmationView = new ConfirmationView({model: reservation});
+  reservation.read();
   reservation.on("error", function(model, error){ smoke.signal(error); });
-  reservation.on("change:isbn", function(model, isbn) { reservation.getUserDetails(); });
-  reservation.on("change:user", function(model, isbn) { reservation.showConfirmation(); });
-  reservation.on("confirmed", newReservation);
-}
+};
 
-document.observe("dom:loaded", function() { newReservation(); })
+$(function() { newReservation(); });
